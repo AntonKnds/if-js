9b707716794cd12471d8f02b6c7514e3f545f345
Anton <antonkundas@gmail.com>
2020-11-25 15:20:15 +0300

    new

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 /*1*/
 function palindrome(p) {
-    for (i = (p.length); i--; i != 0) {
-        for (j = 0; j < p.length; j++) {
-            if (p[i] == p[j]) {
-                return true;
-            }
-            return false;
-        }
+  for (i = (p.length); i--; i != 0) {
+    for (j = 0; j < p.length; j++) {
+      if (p[i] == p[j]) {
+        return true;
+      }
+      return false;
     }
+  }
 }
-
 console.log(palindrome('шалаш'));
 console.log(palindrome('кок'));
 console.log(palindrome('антон'));
 
 /*2*/
 function min(a, b) {
-    if (a < b) {
-        return a;
-    }
-    return b;
+  if (a < b) {
+    return a;
+  }
+  return b;
 }
 
 function max(a, b) {
-    if (a > b) {
-        return a;
-    }
-    return b;
+  if (a > b) {
+    return a;
+  }
+  return b;
 }
 
 console.log(min(3, 4));
@@ -34,7 +33,7 @@ console.log(max(5, 6));
 
 
 function maxMin(a, b) {
-    return a > b ? a : b;
+  return a > b ? a : b;
 }
 
 console.log(maxMin(5, 4));
@@ -44,23 +43,31 @@ console.log(maxMin(5, 4));
 
 
 const myArr = [];
-for (i = 0; i < 100; i++) {
-    myArr.push(Math.round(Math.random() * i));
-}
-console.log(myArr);
 
 let myFunction = function (arr) {
-    for (i = 0; i < arr.length; i++) {
-        let arrSymbol = arr[i].toString();
-        if (arrSymbol.endsWith('0')) {
-            arrSymbol = arrSymbol.replace('0', 'zero');
-            arr[i] = arrSymbol;
-        }
+  for (i = 0; i < 100; i++) {
+    myArr.push(Math.round(Math.random() * i));
+  }
+  console.log(myArr);
+  for (i = 0; i < arr.length; i++) {
+    let arrSymbol = arr[i].toString();
+    if (arrSymbol.endsWith('0')) {
+      arrSymbol = arrSymbol.replace('0', 'zero');
+      arr[i] = arrSymbol;
     }
-    console.log(arr);
+  }
+  console.log(arr);
 }
 
 myFunction(myArr);
+
+
+
+
+
+
+
+
 
 
 
