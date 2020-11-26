69a87ae1c10750157a2e7716df731c5ae2d082ca
AntonKnds <71836376+AntonKnds@users.noreply.github.com>
2020-11-26 15:44:26 +0300

    функции и строки (#3)
    * функции и строки
    
    * new
    
    * lesson-4
    
    * lesson-4 + reverse()
    
    * замыкание
    
    * замыкание.
    
    * push
    
    Co-authored-by: Anton <antonkundas@gmail.com>

--- main.js
+++ main.js
@@ -1,41 +1,115 @@
-let user = "John Doe";
-console.log(user);
-const student = "Anton";
-console.log(student);
-user = student;
-console.log(user); /* output Anton */
-
-let test = 1;
-test += 1;
-console.log(test);/*output 2*/
-test += '1';
-console.log(test);/*output 21*/
-test -= 1;
-console.log(test); /*output 20*/
-test = ' ';
-console.log(!!test); /*output true*/
-
-
-const arr = [2, 3, 5, 8];
-let result = 1;
-for (i = 0; i < arr.length; i++) {
-    result *= arr[i];
-}
-console.log(result);
-
-const arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
-for (i = 0; i < arr2.length; i++) {
-    if (arr2[i] > 5 && arr2[i] < 10) {
-        console.log(arr2[i]);
+function newPalindrome(word) {
+  let newWord = word.split("").reverse();
+  for (i = 0; i < newWord.length; i++) {
+    if (word[i] === newWord [i]) {
+      return true;
     }
+    return false;
+  }
 }
 
-const arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
-for (i = 0; i < arr3.length; i++) {
-    if ((arr3[i] % 2) === 0 && arr3[i] !== 0) {
-        console.log(arr3[i]);
+
+console.log(newPalindrome("anttna"));
+console.log(newPalindrome("oanttnoa"));
+
+/*1*/
+function palindrome(p) {
+  for (i = (p.length); i--; i != 0) {
+    for (j = 0; j < p.length; j++) {
+      if (p[i] == p[j]) {
+        return true;
+      }
+      return false;
     }
+  }
 }
+
+console.log(palindrome('шалаш'));
+console.log(palindrome('кок'));
+console.log(palindrome('антон'));
+
+/*2*/
+function min(a, b) {
+  if (a < b) {
+    return a;
+  }
+  return b;
+}
+
+function max(a, b) {
+  if (a > b) {
+    return a;
+  }
+  return b;
+}
+
+console.log(min(3, 4));
+console.log(max(5, 6));
+
+
+function maxMin(a, b) {
+  return a > b ? a : b;
+}
+
+console.log(maxMin(5, 4));
+
+
+/*3*/
+
+
+const myArr = [];
+
+let myFunction = function (arr) {
+  for (i = 0; i < 100; i++) {
+    myArr.push(Math.round(Math.random() * i));
+  }
+  console.log(myArr);
+  for (i = 0; i < arr.length; i++) {
+    let arrSymbol = arr[i].toString();
+    if (arrSymbol.endsWith('0')) {
+      arrSymbol = arrSymbol.replace('0', 'zero');
+      arr[i] = arrSymbol;
+    }
+  }
+  console.log(arr);
+}
+
+myFunction(myArr);
+
+/* lesson-4 */
+
+/*1*/
+
+function sum(a) {
+  return function (b) {
+    return a + b;
+  }
+}
+
+console.log(sum(5)(4));
+
+/*2*/
+const text1El = document.getElementById("text1");
+const text2El = document.getElementById("text2");
+const text3El = document.getElementById("text3");
+
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+
+text1El.addEventListener('click', addCounter());
+text2El.addEventListener('click', addCounter());
+text3El.addEventListener('click', addCounter());
+
+function addCounter() {
+  let counter = 0;
+  return function (event) {
+    event.target.style.color = colors[counter];
+    counter = counter > 3 ? 0 : counter + 1;
+  }
+};
+
+
+
+
 
 
 
